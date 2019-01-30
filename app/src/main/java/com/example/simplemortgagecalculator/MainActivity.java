c13f825e9ee6fe079fa7ff9c6ed19070abc3bcd4
ketan8O8 <sens_sontakke@hotmail.com>
2019-01-30 00:46:53 -0500

    Added interest, amortization period fields Added calculation module
    doesn't run, crashes when you click calculate

--- app/src/main/java/com/example/simplemortgagecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplemortgagecalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simplemortgagecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,21 +12,52 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText principal, interest, amort;
+    double mortgagePayment;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView principal = (TextView)findViewById(R.id.etPrincipal);
-        Button nextButton = (Button) findViewById(R.id.btNext);
-        nextButton.setOnClickListener(new View.OnClickListener() {
+
+        //Input Edittext :Principle, Interest, Amortization
+        principal = (EditText)findViewById(R.id.editPrinciple);
+        interest = (EditText)findViewById(R.id.editInterest);
+        amort = (EditText)findViewById(R.id.editAmort);
+
+
+
+
+        Button calculateButton = (Button) findViewById(R.id.btCalculate);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                goToNext();
+                goToSummary();
             }
         });
+
+
     }
-   protected void goToNext(){
-       Intent myIntent = new Intent(MainActivity.this, SummaryActivity.class);
-       startActivity(myIntent);
-   }
+
+    public void calculateMortagePayment(){
+        double entPrincipal = Integer.parseInt(principal.getText().toString());
+        double entInterest = Integer.parseInt(interest.getText().toString());
+        double entAmort = Integer.parseInt(amort.getText().toString());
+
+        double r = (entInterest/100)/12;
+        double n = (12*entAmort);
+
+        mortgagePayment = entPrincipal * ((r*(Math.pow(1+r,n)))/((Math.pow(1+r,n)) -1));
+
+
+    }
+
+
+
+    protected void goToSummary(){
+        Intent myIntent = new Intent(MainActivity.this, SummaryActivity.class);
+        myIntent.putExtra("Mortgage_Payment", mortgagePayment);
+        startActivity(myIntent);
+    }
 }
--- app/src/main/java/com/example/simplemortgagecalculator/SummaryActivity.java
+++ app/src/main/java/com/example/simplemortgagecalculator/SummaryActivity.java
@@ -2,12 +2,26 @@ package com.example.simplemortgagecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class SummaryActivity extends AppCompatActivity {
+
+    private TextView result;
+
+    String mortgagePayment = getIntent().getStringExtra("Mortgage_Payment");
+
+    //MainActivity main = new MainActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+
+        //result = (TextView)findViewById(R.id.summaryResult);
+
+        //result.setText("Your monthly mortgage payment is:" + new DecimalFormat("##.##").format(mortgagePayment));
     }
 }
